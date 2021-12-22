a43f41ee30d388015ac1cf76cbc4d079640d64d1
Dinesh Rajput <dinesh@singlife.com>
2021-12-22 10:34:32 +0530

    added second solution with O{n) complexity

--- buySellStock.js
+++ buySellStock.js
@@ -5,7 +5,23 @@
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0. */
 
+//Example 1:
 
+/* Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell. */
+
+//Example 2:
+
+/* Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0. */
+
+
+/* =========================================================================================== */
+
+// SOLUTION 1 
 
 function calculateMaxProfit(prices){
 let maxProfit = 0;
@@ -21,20 +37,24 @@ let profit;
    return maxProfit;
 }
 
-// const prices = [3, 2, 4, 1, 7, 5];
-const prices = [3, 2, 4, 1, 1, 0];
+const prices = [7,1,5,3,6,4];
 console.log( `Max Profit is ${calculateMaxProfit(prices)}`);
 
+//Complexity O(n^2)
 
-//Example 1:
+/* =========================================================================================== */
 
-/* Input: prices = [7,1,5,3,6,4]
-Output: 5
-Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
-Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell. */
+// SOLUTION 2 :
 
-//Example 2:
+function maxProfit(prices){
+    let maxProfit = 0;
+    let min=Infinity;
+    prices.forEach((price, i)=>{
+        min = Math.min(price, min);
+        maxProfit = Math.max(maxProfit, price - min);
+    })
+    return maxProfit;
+}
+console.log(`Max Profit is ${maxProfit([7,2,8,1,6,4])}`)
 
-/* Input: prices = [7,6,4,3,1]
-Output: 0
-Explanation: In this case, no transactions are done and the max profit = 0. */
+//Complexity O(n)
